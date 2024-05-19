2aa5fc3a794729cb859c6ac60b7c2c422c209952
MKS1699 <codermehul@gmail.com>
2024-05-19 13:04:54 +0530

    Fix: <br /> bug

--- app/pages/home/categories/[category]/[article]/components/ArticleDescription.tsx
+++ app/pages/home/categories/[category]/[article]/components/ArticleDescription.tsx
@@ -1,7 +1,18 @@
 const ArticleDescription = ({ description }: { description: string }) => {
+  // removing <br /> from description where it is as a string
+  // and replacing with "/n" because this will never(hope)
+  // be posted in the db.
+  const descriptionRestructured = description.replaceAll("<br />", "/n");
+  // splitting the description based on "/n" into multiple line description
+  const descriptionParaArr = descriptionRestructured.split("/n");
   return (
     <div className="w-full h-auto px-1 text-base lg:text-lg my-2 text-dark dark:text-light">
-      {description}
+      {/* rendering multiple lines (para) of description */}
+      {descriptionParaArr.map((para: string, index: number) => {
+        if (para !== "")
+          return <p key={`article-para-${index}`}>{para}</p>; // renders content
+        else return <br />; // replaces "/n" -> "" with html <br />
+      })}
     </div>
   );
 };
