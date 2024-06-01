8f7eb72f245c758e93895ad73b1c9461e32e0f05
MKS1699 <codermehul@gmail.com>
2024-06-01 19:56:32 +0530

    usePostPublishSave hook created

--- app/hooks/apiHooks/index.ts
+++ app/hooks/apiHooks/index.ts
@@ -3,10 +3,12 @@ import useGetPostsCategorySizes from "./useGetPostsCategorySizes";
 import useGetUpcomingIPOEntries from "./useGetUpcomingIPOEntries";
 import useGetPostsByType from "./useGetPostsByType";
 import useGetPost from "./useGetPost";
+import usePostPublishSave from "./usePostPublishSave";
 export {
   useGetLatestPosts,
   useGetPostsCategorySizes,
   useGetUpcomingIPOEntries,
   useGetPostsByType,
   useGetPost,
+  usePostPublishSave,
 };
