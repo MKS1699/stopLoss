c2d96ba400a051456bf3ba7f85ddaa92fc867497
MKS1699 <codermehul@gmail.com>
2024-06-01 12:15:21 +0530

    spinner component & animation created

---
+++ app/components/Spinner.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { ImSpinner9 } from "react-icons/im";
+import { StyledComponent } from "../types/component_types";
+interface SpinnerPropsTypes extends StyledComponent {}
+
+const Spinner = ({ className }: StyledComponent) => {
+  return <ImSpinner9 className={`animate-spinner ${className}`} />;
+};
+
+export default Spinner;
