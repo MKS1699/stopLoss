5628b11e1f3b9160d3f77934c61cd1ef61655463
MKS1699 <codermehul@gmail.com>
2024-06-10 12:02:47 +0530

    update: new method for creating tags

--- app/pages/admin/adminPanel/components/PostTags.tsx
+++ app/pages/admin/adminPanel/components/PostTags.tsx
@@ -2,53 +2,48 @@
 
 import { useEffect, useState } from "react";
 import { PostTitleBarPropsTypes } from "./PostTitleBar";
-import { PostTag } from ".";
+import { useAppDispatch } from "@/app/hooks";
+import { setPostTags } from "@/app/redux/slice/postSlice";
 
 interface PostTagsPropsTypes extends PostTitleBarPropsTypes {}
 
+// todo :
+// check for duplicate tags and alert the user based on it
+
 const PostTags = ({ screen }: PostTagsPropsTypes) => {
-  const [nTags, setNTags] = useState<number>(0);
-  const [nTagsArr, setNTagsArr] = useState<number[] | []>([]);
+  const dispatch = useAppDispatch();
+  // tags state
+  const [tags, setTags] = useState<string[]>([]); // individual tags
+  const [tagsString, setTagsString] = useState<string>(""); //tag string raw input
 
-  // initial Tags rendering which will be 20
+  // function for turning raw tags string into individual tags
+  function handleTags() {
+    let tagsArr: string[] = tagsString.split(",");
+    setTags(tagsArr);
+  }
+
+  // creating individual tags on tagString update
   useEffect(() => {
-    setNTags(20);
-  }, []);
+    handleTags();
+  }, [tagsString]);
 
-  // function for creating Tags Arr
-  function createTagsArr() {
-    let arr: number[] | [] = [];
-    for (var i = 0; i < nTags; i++) {
-      arr[i] = i;
+  // function for updating individual tags in the raw string
+  useEffect(() => {
+    for (var i = 0; i < tags.length; i++) {
+      dispatch(setPostTags({ tag: tags[i], tagID: i }));
     }
-    setNTagsArr(arr);
-  }
+  }, [tags]);
 
-  // creating nTagsArr every time nTags are incremented
-  useEffect(() => {
-    createTagsArr();
-  }, [nTags]);
-
-  // increment nTagsArr
-  function incrementNTags() {
-    setNTags((prev: number) => prev + 20);
-  }
   return (
-    <div className="w-full h-auto flex flex-col gap-2">
-      <div className="w-full h-auto self-center">Tags : {nTags}</div>
-      <div className="w-full h-auto grid grid-cols-3 md:grid-cols-4 lg:grid-cols-5 xl:grid-cols-6 grid-flow-row gap-1 border-y-2 border-solid border-[#003b31] dark:border-[#4cb050] py-2">
-        {nTagsArr.map((tagID: number) => {
-          return (
-            <PostTag screen={screen} tagID={tagID} key={`Post Tag ${tagID}`} />
-          );
-        })}
-      </div>
-      <div
-        className="w-full h-fit p-1 text-base bg-[#4cb050] bg-opacity-70 text-[#003b31] rounded-md cursor-pointer text-center self-center dark:text-black font-medium mb-2"
-        onClick={incrementNTags}
-      >
-        + Tags
-      </div>
+    <div className="w-full h-auto flex flex-col justify-evenly items-center justify-items-center mb-2 gap-2 border-b-2 border-solid border-[#003b31] dark:border-[#4cb050] pb-2">
+      <div className="font-semibold">Tags</div>
+      <input
+        type="text"
+        className="w-full h-fit p-1 text-base bg-[#4cb050] bg-opacity-50 text-[#003b31] rounded-md cursor-text outline-none placeholder:text-[#003b31] placeholder:text-opacity-70 text-left indent-2"
+        placeholder={`Enter Tags separated by , :-  tag1,tag2 . At least 20 unique tags are required.`}
+        onChange={(e) => setTagsString(e.target.value.toString())}
+        value={tagsString}
+      />
     </div>
   );
 };
