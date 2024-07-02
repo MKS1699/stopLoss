95a2ab3d909a7f70b475a5fbfca98af71914849f
MKS1699 <codermehul@gmail.com>
2024-07-02 18:29:30 +0530

    INR string converter function

--- app/utils/tools.ts
+++ app/utils/tools.ts
@@ -25,3 +25,19 @@ export function createTitleURL(title: string): string {
   const newTitle = titleArr.join("-");
   return newTitle;
 }
+
+// converts a given string into Indian Number System format
+// example : 12345 -> 12,345
+export function createINRString(val: string | number): string {
+  const valArr: string[] = val.toString().split("").reverse();
+  let sumFactor = 1;
+  for (let i: number = 0; i < valArr.length; i++) {
+    if (i > 0 && i % 2 == 0) {
+      if (i + sumFactor < valArr.length) {
+        valArr.splice(i + sumFactor, 0, ",");
+        sumFactor++;
+      }
+    }
+  }
+  return valArr.reverse().join("").toString();
+}
